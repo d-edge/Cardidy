0259c1b3c1ceba5eece2198c060789abed229f7f
Aloïs de Gouvello <aloisdegouvello@live.fr>
2021-10-05 12:31:50 +0200

    fix: move Issuer Identification Number's size to 8

--- Cardidy/Model/PaddedRange.cs
+++ Cardidy/Model/PaddedRange.cs
@@ -2,6 +2,10 @@
 
 internal record PaddedRange
 {
+    private const int identificationNumberLength = 8;
+    private const int minDigit = 0;
+    private const int maxDigit = 9;
+
     internal int Min { get; }
     internal int Max { get; }
 
@@ -9,8 +13,8 @@ internal record PaddedRange
 
     internal PaddedRange(int min, int max)
     {
-        Min = min.PadRight(6, 0);
-        Max = max.PadRight(6, 9);
+        Min = min.PadRight(identificationNumberLength, minDigit);
+        Max = max.PadRight(identificationNumberLength, maxDigit);
     }
 
     internal bool Contains(int value) => value >= Min && value <= Max;
