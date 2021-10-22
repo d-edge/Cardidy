5e7f775862f5ecce893cd435adda247deacd8621
hey24sheep <hey24sheep@gmail.com>
2021-10-22 19:08:06 +0530

    benchmarks added

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine(Dedge.Cardidy.Identify("4127540509730813").Single());
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using Dedge;
+using Tests;
+
+namespace App;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Cardidy.Identify("4127540509730813").Single());
+
+
+        var summary = BenchmarkRunner.Run<BenchmarkCardidy>();
+        
+        Console.WriteLine(summary);
+    }
+}
---
+++ src/App/run_benchmarks.bat
@@ -0,0 +1,1 @@
+dotnet run -p App.csproj -c Release
